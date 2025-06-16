02d8d7dba5aa2a573ad2f30cd040bb6e36d12bd5
patryk <20patryk01c@gmail.com>
2025-06-16 19:14:32 +0200

    Update 3

---
+++ src/app/adminPanel/_components/FilterPanel.tsx
@@ -0,0 +1,10 @@
+// src/app/adminPanel/_components/FilterPanel.tsx
+
+"use client"; // Prawdopodobnie potrzebujesz tej dyrektywy, jeśli używasz hooków
+
+import React from "react";
+
+export default function FilterPanel() {
+  // ...twoja logika i JSX
+  return <div>{/* Zawartość twojego panelu filtrów */}</div>;
+}
