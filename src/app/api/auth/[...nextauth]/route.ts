9725161c034300fee5e63416da475445c743a209
patryk <20patryk01c@gmail.com>
2025-06-17 15:07:20 +0200

    Update 37

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,7 +3,13 @@ import { authOptions } from "@/lib/authOptions";
 
 export const runtime = "edge";
 
-export default function handler(req: Request, res: Response) {
+// Eksportuj funkcje GET i POST zgodnie z wymaganiami Next.js App Router
+export async function GET(request: Request) {
   // @ts-ignore
-  return NextAuth(authOptions)(req, res);
+  return NextAuth(authOptions)(request);
+}
+
+export async function POST(request: Request) {
+  // @ts-ignore
+  return NextAuth(authOptions)(request);
 }
