b7d99afa1753fefc3084064cac0d091bdee3415d
patryk <20patryk01c@gmail.com>
2025-06-17 17:12:25 +0200

    Update 45

--- src/lib/authOptions.ts
+++ src/lib/authOptions.ts
@@ -1,6 +1,8 @@
 // src/lib/authOptions.ts
 import Credentials from "next-auth/providers/credentials";
 import { verifyPassword } from "./crypto";
+import type { JWT } from "next-auth/jwt";
+import type { Session, User } from "next-auth";
 
 export const authOptions = {
   providers: [
@@ -56,11 +58,11 @@ export const authOptions = {
     error: "/auth/error",
   },
   callbacks: {
-    async jwt({ token, user }) {
+    async jwt({ token, user }: { token: JWT; user?: User }) {
       if (user?.role) token.role = user.role;
       return token;
     },
-    async session({ session, token }) {
+    async session({ session, token }: { session: Session; token: JWT }) {
       if (session.user && token.role) {
         (session.user as any).role = token.role;
       }
