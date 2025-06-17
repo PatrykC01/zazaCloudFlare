8c040a9ff0c97e4d2af825d9d60f066d9eb5706b
patryk <20patryk01c@gmail.com>
2025-06-17 15:01:33 +0200

    Update 35

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,7 @@
+import NextAuth from "next-auth";
+import { authOptions } from "@/lib/authOptions";
 
-export const runtime = 'edge'
+export const runtime = "edge";
 
-export { GET, POST } from 'next-auth/app'
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
