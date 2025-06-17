d81cf3ea703b8abadbbff5581796e3633c0526a4
patryk <20patryk01c@gmail.com>
2025-06-17 14:59:04 +0200

    Update 34

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,4 @@
-import NextAuth from 'next-auth'
-import { authOptions } from '@/lib/authOptions'
 
 export const runtime = 'edge'
 
-// To działa tylko w App Router i next-auth v5+
-const handler = NextAuth(authOptions)
-export { handler as GET, handler as POST }
+export { GET, POST } from 'next-auth/app'
