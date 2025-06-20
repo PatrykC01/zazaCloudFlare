8a074f9887de0dd7c7b8f1b00b85dfb9915f7a17
patryk <20patryk01c@gmail.com>
2025-06-20 15:52:26 +0200

    Proba naprawy cms 15

--- src/components/IntroSection.tsx
+++ src/components/IntroSection.tsx
@@ -1,58 +1,37 @@
 // Plik: src/components/IntroSection.tsx
-"use client";
 
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-const IntroSection: React.FC = () => {
-    // 1. Zmień stan początkowy na null lub pusty obiekt {}
-    const [content, setContent] = useState<any>(null);
-    const [loading, setLoading] = useState(true);
+// 1. Zdefiniuj interfejs dla przyjmowanych właściwości (props)
+interface IntroSectionProps {
+  text: string;
+  imageUrl: string;
+}
 
-    useEffect(() => {
-        const fetchContent = async () => {
-            try {
-                const response = await fetch('/api/content');
-                const data = await response.json();
-                setContent(data);
-            } catch (error) {
-                console.error('Failed to fetch content:', error);
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        fetchContent();
-    }, []);
-
-    if (loading) {
-        return <div>Ładowanie...</div>; // Opcjonalnie, wskaźnik ładowania
-    }
-
-    // 2. Zmień sposób dostępu do danych
-    const aboutP = content?.AboutP;
-    const aboutImg = content?.AboutIMG;
-
-    return (
-        <div id="intro" className="basic-1">
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-5">
-                        <div className="text-container">
-                            <div className="section-title">O NAS</div>
-                            {/* Użyj zmiennych bezpośrednio */}
-                            <p>{aboutP || 'Brak treści AboutP'}</p>
-                        </div>
-                    </div>
-                    <div className="col-lg-7">
-                        <div className="image-container">
-                            {/* Użyj zmiennej bezpośrednio */}
-                            <img className="img-fluid" src={aboutImg || 'images/intro-office.jpg'} alt="alternative" />
-                        </div>
-                    </div>
-                </div>
+// 2. Użyj zdefiniowanego interfejsu w komponencie
+const IntroSection: React.FC<IntroSectionProps> = ({ text, imageUrl }) => {
+  return (
+    <div id="intro" className="basic-1">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-5">
+            <div className="text-container">
+              <div className="section-title">O NAS</div>
+              {/* 3. Użyj propsów bezpośrednio w JSX */}
+              <p>{text || 'Brak treści AboutP'}</p>
+             
             </div>
+          </div>
+          <div className="col-lg-7">
+            <div className="image-container">
+              {/* 4. Użyj propsów bezpośrednio w JSX */}
+              <img className="img-fluid" src={imageUrl || 'images/intro-office.jpg'} alt="O nas" />
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default IntroSection;
